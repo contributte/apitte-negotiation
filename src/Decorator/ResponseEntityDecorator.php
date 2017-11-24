74826f88f6bcaadd2a6670404f2e77f2ee007abe
Milan Felix Šulc <rkfelix@gmail.com>
2017-11-24 11:14:51 +0100

    Bugfix: dont apply ResponseEntityDecorator in case of no entity given

--- src/Decorator/ResponseEntityDecorator.php
+++ src/Decorator/ResponseEntityDecorator.php
@@ -31,6 +31,14 @@ class ResponseEntityDecorator implements IDecorator
 	 */
 	public function decorate(ServerRequestInterface $request, ResponseInterface $response, array $context = [])
 	{
+		// Skip if response is not our ApiResponse
+		if (!($response instanceof ApiResponse)) return $response;
+
+		// Skip if there's no entity and no $context, it does not make sence
+		// to negotiate response without entity.
+		// Except if there's exception in $context.
+		if ($response->getEntity() === NULL && empty($context)) return $response;
+
 		return $this->negotiation->negotiate($request, $response, $context);
 	}
 
